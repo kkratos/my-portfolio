21c4063e37c092be05adb6e84565f124b6a32617
kamal patel <kamal.patel30@hotmail.com>
2021-01-18 20:55:29 +0530

    home page image error

--- src/components/Home.js
+++ src/components/Home.js
@@ -45,7 +45,7 @@ function Home() {
     <main className="lg:w-11/12 max-w-screen-lg m-auto p-8 my-7">
       <section className="md:flex mt-10">
         <div className="flex justify-center">
-          <img src={image} alt="profile" className="w-48 rounded-full my-5 md:m-0" />
+          <img src={image} alt="profile" className="object-contain w-48 rounded-full my-5 md:m-0" />
         </div>
 
         <div className="md:flex md:flex-col justify-center">
